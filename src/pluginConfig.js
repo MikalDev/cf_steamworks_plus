380e763c67a7ccbeadd08d55274a1e24271c8166
MikalDev <33675273+MikalDev@users.noreply.github.com>
2024-10-30 18:32:13 -0700

    update example and plugin version
    this version includes linux cef support

--- src/pluginConfig.js
+++ src/pluginConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   addonType: "plugin",
   id: "cf_steamworks_plus",
   name: "Steamworks Plus",
-  version: "1.8.3",
+  version: "1.9.0",
   category: "platform-specific",
   author: "cf",
   website: "https://www.construct.net",
